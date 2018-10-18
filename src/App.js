8761bec7c95dedc76131fef5700fb9c0bc44b78a
Minh PHAM2 <mpham@e1r6p6.42.fr>
2018-10-18 19:01:28 +0200

    homepage

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import Header from './components/Header';
+import HomePage from './layouts/HomePage';
 // import LandingPage from './layouts/LandingPage';
 // import NotFound from './layouts/NotFound';
 
@@ -13,6 +14,7 @@ class App extends Component {
                   <Header/>
                   <Switch>
                       {/* <Route exact path="/" component={LandingPage} /> */}
+                      <Route path="/homepage" component={HomePage} />
                       {/* <Route component={NotFound} /> */}
                   </Switch>
               </div>
