3b710eb0e32209c73d52e932fcad2b8f2db9bc7b
Minh PHAM2 <mpham@e1r12p21.42.fr>
2018-11-14 19:51:00 +0100

    fix

--- server/data/seeds/eztv.js
+++ server/data/seeds/eztv.js
@@ -3,63 +3,73 @@ const fs = require('fs');
 const axios = require('axios');
 
 const throttledQueue = require('throttled-queue');
-const throttle = throttledQueue(39, 10000);
+const throttle = throttledQueue(1, 1000);
 
-function getAllMoviesFromEZTV(i) {
-    axios.get('https://eztv.ag/api/get-torrents?page=' + i)
-        .then((res) => {
-            let k = 0;
-            if (res.status === 200) {
-                for (var j = 0; j < res.data.torrents.length; j++) {
-                    let data = new Object();
-                    let movie = res.data.torrents[j];
-                    if (movie.torrents_url || movie.magnet_url && movie.imdb_id) {
-                        throttle(function () {
-                            axios.get('http://www.omdbapi.com/?i=tt' + movie.imdb_id + '&apikey=' + keys.OMDB_API_KEY)
-                                .then((res) => {
-                                    data['id'] = movie.id;
-                                    data['title'] = res.data.Title;
-                                    data['sypnosis'] = res.data.Plot;
-                                    data['runtime'] = res.data.Runtime;
-                                    data['year'] = res.data.Year;
-                                    data['genres'] = res.data.Genre;
-                                    data['type'] = res.data.Type;
-                                    data['imdb_rating'] = res.data.imdbRating;
-                                    data['imdb_id'] = 'tt' + movie.imdb_id;
-                                    data['image'] = res.data.Poster;
-                                    data['director'] = res.data.Director;  
-                                    data['writer'] = res.data.Writer;  
-                                    data['actors'] = res.data.Actors;
-                                    let torrents = new Array();
-                                    torrents.push({
-                                        "url" : movie.torrent_url,
-                                        "magnet" : movie.magnet_url,
-                                        "hash" : movie.hash,
-                                        "quality" : movie.filename,
-                                        "seeds" : movie.seeds,
-                                        "peers" : movie.peers,
-                                        "size_bytes": movie.size_bytes
-                                    })
-                                    data['torrents'] = torrents;
-                                    // console.log(data);
-                                    fs.appendFileSync("movies.json", JSON.stringify(data), 'utf8');
-                                })
-                                .catch(error => {
-                                    console.log(error.response)
-                                });
-                        });
-                    }
+async function getAllMoviesFromEZTV(i) {
+    try {
+        const res = await axios.get('https://eztv.ag/api/get-torrents?page=' + i);
+        if (res.status === 200) {
+            for (var j = 0; j < res.data.torrents.length; j++) {
+                let data = new Object();
+                let movie = res.data.torrents[j];
+                if (movie.torrents_url || movie.magnet_url && movie.imdb_id) {
+                    throttle(async function () {
+                        try {
+                            const res = await axios.get('http://www.omdbapi.com/?i=tt' + movie.imdb_id + '&apikey=' + keys.OMDB_API_KEY);
+                            const res2 = await axios.get('https://api.themoviedb.org/3/find/tt' + movie.imdb_id + '?api_key=' + keys.TMDB_API_KEY + '&external_source=imdb_id');
+                            data['id'] = movie.id;
+                            data['title'] = res.data.Title;
+                            data['sypnosis'] = res.data.Plot;
+                            data['runtime'] = res.data.Runtime;
+                            data['year'] = res.data.Year;
+                            data['genres'] = res.data.Genre;
+                            data['type'] = res.data.Type;
+                            data['imdb_rating'] = res.data.imdbRating;
+                            data['imdb_id'] = 'tt' + movie.imdb_id;
+                            data['director'] = res.data.Director;  
+                            data['writer'] = res.data.Writer;  
+                            data['actors'] = res.data.Actors;
+                            let torrents = new Array();
+                            torrents.push({
+                                "url" : movie.torrent_url,
+                                "magnet" : movie.magnet_url,
+                                "hash" : movie.hash,
+                                "quality" : movie.filename,
+                                "seeds" : movie.seeds,
+                                "peers" : movie.peers,
+                                "size_bytes": movie.size_bytes
+                            })
+                            data['torrents'] = torrents;
+                            if (res2.data.movie_results.length > 0) {
+                                if (res2.data.movie_results[0].poster_path !== null) {
+                                    data['image'] = 'https://image.tmdb.org/t/p/w780' + res2.data.movie_results[0].poster_path;
+                                } else {
+                                    data['image'] = 'N/A';
+                                }
+                            } else if (res2.data.tv_results.length > 0) {
+                                if (res2.data.tv_results[0].poster_path !== null) {
+                                    data['image'] = 'https://image.tmdb.org/t/p/w780' + res2.data.tv_results[0].poster_path;
+                                } else {
+                                    data['image'] = 'N/A';
+                                }
+                            }
+                            console.log(data);
+                            fs.appendFileSync("movies.json", JSON.stringify(data), 'utf8');
+
+                        } catch (err) { 
+                            console.log(err.response)
+                        }  
+                    })
                 }
-                i++;
-                getAllMoviesFromEZTV(i);
             }
-            else {
-                console.log('done')
-            }
-        })
-        .catch(error => {
-            console.log(error.response)
-        });
+            i++;
+            getAllMoviesFromEZTV(i);
+        } else {
+            console.log('done');
+        }
+    } catch (err) { 
+        console.log(err.response)
+    }
 }
 
 var i = 1;
--- server/services/elasticsearch.js
+++ server/services/elasticsearch.js
@@ -1,78 +1,78 @@
-// const index = 'hypertube';
-// const type = 'movies';
-// const fs = require('fs');
-// const JSONStream = require("JSONStream");
-// const elasticsearch = require('elasticsearch');
+const index = 'hypertube';
+const type = 'movies';
+const fs = require('fs');
+const JSONStream = require("JSONStream");
+const elasticsearch = require('elasticsearch');
 
-// const client = new elasticsearch.Client({  
-//   host: '192.168.99.100:9200',
-//   log: 'trace'
-// });
+const client = new elasticsearch.Client({  
+  host: '192.168.99.100:9200',
+  log: 'trace'
+});
 
-// client.ping({
-//   requestTimeout: 30000,
-// }, function(err) {
-//   if (err) {
-//       console.error('Elasticsearch cluster is down!');
-//   } else {
-//       console.log('Everything is ok');
-//   }
-// });
+client.ping({
+  requestTimeout: 30000,
+}, function(err) {
+  if (err) {
+      console.error('Elasticsearch cluster is down!');
+  } else {
+      console.log('Everything is ok');
+  }
+});
 
-// client.cluster.health({},function(err,resp,status) {  
-//   console.log("-- Client Health --",resp);
-// });
+client.cluster.health({},function(err,resp,status) {  
+  console.log("-- Client Health --",resp);
+});
 
-// function readStream(callback) {
-//   let bulk = [];
+function readStream(callback) {
+  let bulk = [];
 
-//   let stream = fs.createReadStream('./data/seeds/yify.json');
-//   let jsonStream = JSONStream.parse('*');
-//   stream.pipe(jsonStream);
-//   jsonStream.on('data', function(data) {
-//     bulk.push({index:{
-//         _index:'hypertube',
-//         _type:'movies'
-//       }
-//     })
-//     bulk.push(data);
-//     console.log("BULKKKKKKKKKK", bulk)
-//   })
-//   jsonStream.on('end', function() {
-//     callback(bulk);
-//   })
-// }
+  let stream = fs.createReadStream('./data/seeds/yify.json');
+  let jsonStream = JSONStream.parse('*');
+  stream.pipe(jsonStream);
+  jsonStream.on('data', function(data) {
+    bulk.push({index:{
+        _index:'hypertube',
+        _type:'movies'
+      }
+    })
+    bulk.push(data);
+    console.log("BULKKKKKKKKKK", bulk)
+  })
+  jsonStream.on('end', function() {
+    callback(bulk);
+  })
+}
 
-// // async function insertMovieMapping () {
-// //   const schema = {
-// //     title: { type: 'keyword' },
-// //     author: { type: 'keyword' },
-// //     location: { type: 'integer' },
-// //     text: { type: 'text' }
-// //   }
-
-// //   return client.indices.putMapping({ index, type, body: { properties: schema } })
-// // }
-
-// async function resetIndex() {
-//   if (await client.indices.exists({ index })) {
-//     await client.indices.delete({ index })
+// async function insertMovieMapping () {
+//   const schema = {
+//     title: { type: 'keyword' },
+//     author: { type: 'keyword' },
+//     location: { type: 'integer' },
+//     text: { type: 'text' }
 //   }
 
-//   await client.indices.create({ index });
-//   // await insertMovieMapping()
-
-//   readStream(function(bulk) {
-//     client.bulk({body:bulk}, function(err, res) { 
-//       if(err) { 
-//           console.log("Failed Bulk operation") ;
-//       } else { 
-//           console.log("Successfully imported " + bulk.length + " movies"); 
-//       } 
-//     }); 
-//   });
+//   return client.indices.putMapping({ index, type, body: { properties: schema } })
 // }
 
-// resetIndex();
+async function resetIndex() {
+  if (await client.indices.exists({ index })) {
+    await client.indices.delete({ index })
+  }
 
-// module.exports = client;
+  await client.indices.create({ index });
+  // await insertMovieMapping()
+
+  readStream(function(bulk) {
+    client.bulk({body:bulk}, function(err, res) { 
+      if(err) { 
+          console.log("Failed Bulk operation") ;
+      } else { 
+          console.log("Successfully imported " + bulk.length + " movies"); 
+      } 
+    }); 
+  });
+}
+
+resetIndex();
+
+module.exports = client;
