2a7480ba8f856d66c35935a52fdfad4b3991ccb9
Leo-taro FUJIMOTO <lfujimot@e1r9p14.42.fr>
2018-11-15 18:35:14 +0100

    xsrf persistent after page reload

--- client/src/index.js
+++ client/src/index.js
@@ -11,6 +11,7 @@ import {I18nextProvider} from 'react-i18next';
 import i18next from 'i18next';
 import common_en from "./translations/en/common.json";
 import common_fr from "./translations/fr/common.json";
+import Reload from './utils/HOC/Reload';
 
 const { persistor, store } = configureStore();
 
@@ -30,9 +31,11 @@ i18next.init({
 ReactDOM.render(
     <I18nextProvider i18n={i18next}>
         <Provider store={store}>
-            <PersistGate loading={null} persistor={persistor}>
-                <App />
-            </PersistGate>
+            <Reload>
+                <PersistGate loading={null} persistor={persistor}>
+                    <App />
+                </PersistGate>
+            </Reload>
         </Provider>
     </I18nextProvider>
     , document.getElementById('root')
