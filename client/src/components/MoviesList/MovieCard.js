c7116e435f66269df69dfa00e4ec20a285965d77
Audrey LAMY <alamy@e1r11p23.42.fr>
2018-11-19 14:52:30 +0100

    gallery movies fixed

--- client/src/components/MoviesList/MovieCard.js
+++ client/src/components/MoviesList/MovieCard.js
@@ -56,7 +56,7 @@ class MovieCard extends Component {
     render() {
         const { movie, showMovieDetails } = this.props;
         return (
-            <div className="pointer">
+            <div className="pointerBis">
                     {this.renderImage(movie, showMovieDetails)}
                 <div className="movies-list-container__info">
                     {this.renderBasicInfo(movie)}
